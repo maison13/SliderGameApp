4a8f0a96014a7a718804d0db3e737e6eb6753e17
Just Vovo <mr.visitka@gmail.com>
2023-06-18 23:29:12 +0300

    Initial Commit

---
+++ SliderGameApp/SliderGameAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderGameAppApp.swift
+//  SliderGameApp
+//
+//  Created by Just Vovo on 18.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct SliderGameAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
